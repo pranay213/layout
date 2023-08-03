f7b6ad98d0ef2c47899ab0f47014931441018f9e
pranaykumarkodam <pranay.k@blocmatrix.com>
2023-08-03 18:27:05 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
-import layout from "../src/svg/3P.svg";
+// import layout from "../src/svg/3P.svg";
 // import Map from "./svg/layout.svg";
 import React from "react";
+import ThirdPart from "./svg/ThirdPart";
 // const SVGMap = React.lazy(() => import("../src/svg/3P.svg"));
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={layout} alt="logo" />
+        {/* <img src={layout} alt="logo" /> */}
+        <ThirdPart />
         {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p> */}
