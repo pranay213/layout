8ad2a25b5bc891016026269bc4a03b729f4569f2
pranaykumarkodam <pranay.k@blocmatrix.com>
2023-10-04 13:20:57 +0530

    update points

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1046,7 +1046,7 @@ const Layout = (props) => {
     {
       id: 2,
       width: "3.2%",
-      height: "5.4%",
+      height: "5.5%",
       left: "65.9%",
       top: "63.3%",
       transform: "rotate(-12deg)",
