d5ea91fd3db0d899016cf562ed8a701839d49c4a
pranay213 <pranaykodam.213@gmail.com>
2024-03-04 16:32:03 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import Loader from "./Components/Loader";
 import LoaderPage from "./Components/LoaderPage";
 import { UserContext, UsercontextProvider } from "./context";
 import { allDevices } from "./api";
+// import { stopReportingRuntimeErrors } from "react-error-overlay";
 // import Layout from "./Components/Layout";
 const Layout = lazy(() => import("./Components/Layout"));
 const Layout2 = lazy(() => import("./Components/Layout"));
+
+// if (process.env.NODE_ENV === "development") {
+//   import("react-error-overlay").then((m) => {
+//     m.stopReportingRuntimeErrors();
+//   });
+// }
 
 const App = () => {
   // const socket = io();
--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,10 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 // export const BaseUrl2 = "http://192.236.161.98:8085/";
-export const BaseUrl2 = "http://65.0.141.176:8000/api/";
+
+export const BaseUrl2 = "http://192.168.80.225:8000/api/";
+
+// export const BaseUrl2 = "http://65.0.141.176:8000/api/";
 
 export const ApiCall = axios.create({
   baseURL: BaseUrl2,
@@ -11,19 +14,31 @@ export const ApiCall = axios.create({
   headers: { "Content-Type": "application/json" },
 });
 
-ApiCall.interceptors.response.use((res) => {
-  if (res.status >= 400) {
-    toast.error("Something went wrong");
-  }
+// ApiCall.interceptors.response.use((res) => {
+//   if (res.status >= 400) {
+//     toast.error("Something went wrong");
+//   }
 
-  return res;
-  // console.log("res", res);
-});
+//   return res;
+//   // console.log("res", res);
+// });
 
 export const allDevices = async (gatewayId, token) => {
   try {
     ApiCall.defaults.headers.common["isaayu-user-auth"] = token;
     let res = await ApiCall.get(`/gateway/${gatewayId}/devices`);
+    return res?.data;
+  } catch (err) {
+    toast.error("Something went wrong");
+
+    // return { status: "FAIL" };
+  }
+};
+
+export const flowStatus = async (gatewayId, token) => {
+  try {
+    ApiCall.defaults.headers.common["isaayu-user-auth"] = token;
+    let res = await ApiCall.get(`/gateway/flow-status/${gatewayId}`);
     return res?.data;
   } catch (err) {
     toast.error("Something went wrong");
